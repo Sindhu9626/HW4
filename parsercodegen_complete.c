7bdce68c9b3d7637e4c8c98c7abb21ab617296c3
sindhuja sesham <sindhuja.sesham13@gmail.com>
2025-11-08 15:23:32 -0500

    adding and setting up files

---
+++ parsercodegen_complete.c
@@ -0,0 +1,651 @@
+/*
+Assignment:
+HW4 - Complete Parser and Code Generator for PL/0
+(with Procedures, Call, and Else)
+
+Author(s): Sindhuja Sesham, Mikayla Philpot
+
+Language: C (only)
+
+To Compile:
+
+Scanner:
+gcc -O2 -std=c11 -o lex lex.c
+
+Parser/Code Generator:
+gcc -O2 -std=c11 -o parsercodegen_complete parsercodegen_complete.c
+
+Virtual Machine:
+gcc -O2 -std=c11 -o vm vm.c
+
+To Execute (on Eustis):
+./lex <input_file.txt>
+./parsercodegen_complete
+./vm elf.txt
+
+where:
+<input_file.txt> is the path to the PL/0 source program
+
+Notes:
+- lex.c accepts ONE command-line argument (input PL/0 source file)
+- parsercodegen_complete.c accepts NO command-line arguments
+- Input filename is hard-coded in parsercodegen_complete.c
+- Implements recursive-descent parser for extended PL/0 grammar
+- Supports procedures, call statements, and if-then-else
+- Generates PM/0 assembly code (see Appendix A for ISA)
+- VM must support EVEN instruction (OPR 0 11)
+- All development and testing performed on Eustis
+
+Class: COP3402 - System Software - Fall 2025
+
+Instructor: Dr. Jie Lin
+
+Due Date: Friday, November 21, 2025 at 11:59 PM ET
+*/
+#define MAX_SYMBOL_TABLE_SIZE 500
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+// Struct Declarations
+typedef struct {
+    int kind; // const = 1, var = 2, proc = 3
+    char name[12]; // name up to 11 chars
+    int val; // number (ASCII value)
+    int level; // L level
+    int addr; // M address
+    int mark; // to indicate unavailable or deleted
+} symbol;
+
+typedef struct{
+    int OP;
+    int L;
+    int M;
+} instruction;
+
+symbol symbol_table[MAX_SYMBOL_TABLE_SIZE];
+
+instruction instructionSet[MAX_SYMBOL_TABLE_SIZE];
+
+FILE *fp, *outputFile, *readFile;
+int nextToken; 
+int tp;
+int symbCount;
+int cx = 0; // code index, increments by one each time an instruction is stored
+
+// Function Prototypes
+void program();
+void block();
+void constDeclaration();
+int varDeclaration();
+void statement();
+void condition(); 
+void expression();
+void term();
+void factor();
+int findSymbol(char * identifier);
+void insertSymbol(int kind, char * identifier, int val, int level, int addr);
+void deleteSymbol(char * identifier);
+void printAssemblyCode();
+void printSymbolTable();
+char * determineOpcode(int i);
+void error (int errorNumber);
+void getNextToken();
+void emit(int OP, int L, int M);
+
+//Emit function
+void emit(int OP, int L, int M){
+    if (cx > 500) {
+        error(16);
+    }
+    instructionSet[cx].OP = OP;
+    instructionSet[cx].L = L;
+    instructionSet[cx].M = M;
+    cx++;
+}
+
+// Grammar Functions
+void program() {
+    block();
+    // next token should be periodsym
+    if (nextToken != 18) {
+        error(1);
+    }
+    emit(9, 0, 3); // emit halt
+}
+
+void block () {
+    constDeclaration();
+    int numsVars = varDeclaration();
+    // allocates memory on stack for dynamic and static link, return address, and variables
+    emit(6, 0, 3 + numsVars);
+    statement();
+}
+
+void constDeclaration () {
+    // checks if a constant is being declared
+    if(nextToken == 28) {
+        char * identifier = malloc(sizeof(char)*12);
+        int value;
+        // loops because comma w/ more identifiers can occur 0 or more times
+        do { 
+            getNextToken(); // get next token
+            // next token should be identifier
+            if (nextToken != 2) {
+                error(2);
+            }
+            // get identifier
+            fscanf(fp, "%s", identifier);
+            // Symbol name should not have been previously declared
+            if (findSymbol(identifier) != -1) {
+                error(3);
+            }
+            getNextToken();
+            // next token should be equal symbol, to assign value to const
+            if(nextToken != 8) {
+                error(4);
+            }
+            getNextToken();
+            // next token should be numsym
+            if (nextToken != 3) {
+                error(5);
+            }
+            // get number values
+            fscanf(fp, "%d", &value);
+            // insert symbol name into table
+            insertSymbol(1, identifier, value, 0, 0);
+            getNextToken();
+        } while (nextToken == 16); // if next symbol is comma there's another const
+        // next symbol should be semicolon
+        if (nextToken != 17) {
+            error(6);
+        }
+        // get next token to check
+        getNextToken();
+    }
+}
+
+int varDeclaration () {
+    // tracks number of variables for memory allocation
+    int numVars = 0;
+    // checks if a var is being declared
+    if (nextToken == 29) {
+        char * identifier = malloc(sizeof(char)*12);
+        do {
+            numVars++;
+            getNextToken();
+            // next token should be identifier
+            if (nextToken != 2) {
+                error(2);
+               
+            }
+            fscanf(fp, "%s", identifier);
+            // identifier shouldn't already be in symbol table 
+            if(findSymbol(identifier) != -1) {
+                error(3);
+            }
+            // insert identifier into symbol table
+            insertSymbol(2, identifier, 0, 0, numVars + 2);
+            getNextToken();
+        } while(nextToken == 16); // if next symbol is comma, another variable is being declared
+
+        // next symbol should be semicolon
+        if (nextToken != 17) {
+            error(6);
+        }
+        // get next token to check
+        getNextToken();
+
+    }
+    // returns amount of variables to allocate memory for
+    return numVars;
+}
+
+void statement () {
+    char * identifier = malloc(sizeof(char)*12);
+    // checks if an identifier statement
+    if (nextToken == 2) {
+        fscanf(fp, "%s", identifier);
+        
+        int symIndex = findSymbol(identifier);
+        // identifier should've been previously declared
+        if (symIndex == -1) {
+            error(7);
+        }
+        // identifier needs to be a variable
+        if (symbol_table[symIndex].kind != 2) {
+            error(8);
+        }
+        // identifier should not have been marked as inactive
+        if(symbol_table[symIndex].mark != 0){
+            error(17);
+        }
+        getNextToken();
+        // next token should be become symbol
+        if (nextToken != 19) {
+            error(9);
+        }
+        getNextToken();
+        // addresses expression being assigned to identifier
+        expression();
+        // store in variable location
+        emit(4, 0, symbol_table[symIndex].addr);
+        return;
+    }
+    // checks if it's a begin symbol statement
+    if(nextToken == 20) {
+        // addresses all statements inside begin and end
+        do {
+            getNextToken();
+            statement();
+        } while (nextToken == 17); // if there's another statement, should be followed by semicolon
+        // after all statements in begin, end symbol expected
+        if (nextToken != 21) {
+            error(10);
+        }
+        // gets next token to be addressed
+        getNextToken();
+        // returns back to calling function
+        return;
+    }
+    // check for ifsym
+    if(nextToken == 22) {
+        getNextToken();
+        // addresses condition given to if
+        condition();
+        // store index JPC instruction will be at
+        int jpcIndex = cx;
+        // emit inital JPC instruction
+        emit (8, 0, 0);
+        // then expected after if condition
+        if (nextToken != 24) {
+            error(11);
+        }
+        getNextToken();
+        statement();
+        // change memory of JPC instruction to location of first instruction outside of if
+        instructionSet[jpcIndex].M = cx*3;
+        getNextToken();
+        // needs to be an fi after if statement ends
+        if(nextToken != 23) {
+            error(19);        
+        }
+        getNextToken();
+        return;
+    }
+    // check for whilesym
+    if(nextToken == 25) {
+        getNextToken();
+        // store index of condition instruction
+        int loopIndex = cx*3;
+        condition();
+        if (nextToken != 26) {
+            error(12);
+        }
+        getNextToken();
+        int jpcIndex = cx;
+        // jumps past loop if condition isn't met
+        emit (8, 0, 0);
+        statement();
+        // unconditional jump back to condition instruction
+        emit(7, 0, loopIndex);
+        // store location of first non-loop instruction in M of JPC instruction
+        instructionSet[jpcIndex].M = cx*3;
+        return;
+    }
+    // check for readsym
+    if (nextToken == 32) {
+        getNextToken();
+        // identifier expected
+        if (nextToken != 2) {
+            error(2);
+        }
+        // get identifier
+        fscanf(fp, "%s", identifier);
+        int symIndex = findSymbol(identifier);
+        // make sure identifier has been previously declared
+        if (symIndex == -1) {
+            error(7);
+        }
+        // identifier should be variable type
+        if (symbol_table[symIndex].kind != 2) {
+            error(8);
+        }
+        // make sure identifier isn't marked inactive
+        if(symbol_table[symIndex].mark != 0){
+            error(17);
+        }
+        getNextToken();
+        // read from stdin
+        emit(9, 0, 2);
+        // store in variable
+        emit(4, 0, symbol_table[symIndex].addr);
+        return;
+    }
+    // check if write sym
+    if(nextToken == 31) {
+        getNextToken();
+        // called for expression being written
+        expression();
+        // emit write instruction
+        emit(9, 0, 1);
+        return;
+    }
+}
+
+void condition() { 
+    // if even sym 
+    if(nextToken == 34){
+        getNextToken();
+        expression();
+        emit(2, 0, 11); //EVEN
+    }
+    else {
+        // gets expression, then checks what symbol
+        expression();
+        if(nextToken == 8){
+            getNextToken();
+            expression();
+            emit(2, 0, 5); //EQl
+        }
+        else if(nextToken == 9){
+            getNextToken();
+            expression();
+            emit(2, 0, 6); //NEQ
+        }
+        else if(nextToken == 10){
+            getNextToken();
+            expression();
+            emit(2, 0, 7); //LSS
+        }
+        else if(nextToken == 11){
+            getNextToken();
+            expression();
+            emit(2, 0, 8); //LEQ
+        }
+        else if(nextToken == 12){
+            getNextToken();
+            expression();
+            emit(2, 0, 9); //GTR
+        }
+        else if(nextToken == 13){
+            getNextToken();
+            expression();
+            emit(2, 0, 10); //GEQ
+        }
+        else{
+            // condition error, not one of the symbols expected
+            error(13);
+        }
+    }
+    
+}
+
+void expression() {
+    // call term
+    term();
+    // check if add or minus symbol present
+    while(nextToken == 4 || nextToken == 5){
+        if(nextToken == 4){
+            getNextToken();
+            term();
+            emit(2, 0, 1); //ADD
+        }
+        else{
+            getNextToken();
+            term();
+            emit(2, 0, 2); //SUB
+        }
+    }
+
+}
+
+void term(){
+    // calls factor
+    factor();
+    // if multiply or divide symbol present
+    while(nextToken == 6 || nextToken == 7){
+        if(nextToken == 6){
+            getNextToken();
+            factor();
+            emit(2, 0, 3); //MUL
+        }else{
+            getNextToken();
+            factor();
+            emit(2, 0, 4); //DIV
+        }
+    }
+
+}
+
+void factor(){
+
+    char * identifier = malloc(sizeof(char)*12);
+    int value; 
+
+    // check if identifier symbol
+    if(nextToken == 2){
+
+        fscanf(fp, "%s", identifier);
+        int symIdx = findSymbol(identifier);
+
+        if(symIdx == -1){
+            error(7);
+        }
+        else if(symbol_table[symIdx].mark != 0){
+            error(17);
+        }
+        else if(symbol_table[symIdx].kind == 1){
+            emit(1, 0, symbol_table[symIdx].val); //LIT 
+        }
+        else{
+            emit(3, 0, symbol_table[symIdx].addr); //LOD 
+        }
+        getNextToken();
+    }
+    // check if number symbol
+    else if(nextToken == 3){
+        fscanf(fp, "%d", &value); // getting num value
+        emit(1, 0, value); //LIT
+        getNextToken();
+    }
+    // check if left parentheses symbol
+    else if(nextToken == 14){
+        getNextToken();
+        expression();
+        // expecting right parentheses after left parentheses
+        if(nextToken != 15){
+            error(14);
+        }
+        getNextToken();
+    }
+    else{
+        // unexpected input for arithmetic equation
+        error(15);
+    }
+
+}
+
+// Symbol Table Functions
+
+int findSymbol(char * identifier) {
+    int i;
+    for(i = tp; i >= 0; i--) {
+        if(strcmp(symbol_table[i].name, identifier) == 0) {
+            return i;
+        }
+    }
+    return i;
+}
+
+void insertSymbol(int kind, char * identifier, int val, int level, int addr){
+    if(findSymbol(identifier) == -1 ){
+        symbol s1;
+        // if constant
+        if (kind == 1){
+            s1.kind = kind;
+            strcpy(s1.name, identifier);
+            s1.val = val;
+            s1.level = level;
+            // declare address as zero as placeholder for unused category
+            s1.addr = 0;
+            s1.mark = 0;
+        }
+        // if variable
+        else if(kind == 2){
+            s1.kind = kind;
+            strcpy(s1.name, identifier);
+            // declare value as zero as placeholder for unused category
+            s1.val = 0;
+            s1.level = level;
+            s1.addr = addr;
+            s1.mark = 0;
+        }else {
+            // expecting variable or constant
+            error(18);
+        }
+        
+        // place in next available slot on symbol table
+        symbol_table[tp] = s1;
+        tp++;
+        symbCount++;
+        
+    }
+    
+}
+
+void deleteSymbol(char * identifier){
+
+    for(int i = tp; i >= 0; i--) {
+        // find identifier's location on symbol table
+        if(strcmp(symbol_table[i].name, identifier) == 0) {
+            // change mark to 1
+            symbol_table[i].mark = 1;
+        }
+    }
+    tp--;
+}
+
+// change mark of all symbols to 1
+void deleteAll() {
+    for(int i = tp; i >= 0; i--){
+        deleteSymbol(symbol_table[i].name);
+    }
+}
+
+// Print Functions
+void printAssemblyCode() {
+    printf("Assembly code: \n");
+    printf("\nLine\tOP\tL\tM");
+    for (int i = 0; i < cx; i++) {
+        printf("\n%d", i);
+        // print instruction name based on its opcode
+        printf("\t%s", determineOpcode(i));
+        // Print L
+        printf("\t%d", instructionSet[i].L);
+        // Print M
+        printf("\t%d", instructionSet[i].M);
+
+        // printing to file
+        fprintf(outputFile, "%d", instructionSet[i].OP);
+        fprintf(outputFile, " %d", instructionSet[i].L);
+        fprintf(outputFile, " %d\n", instructionSet[i].M);
+    }
+    printf("\n\n");
+}
+
+void printSymbolTable() {
+    
+    printf("Symbol Table:\n\n");
+
+    printf("Kind | Name\t | Value | Level | Address | Mark\n");
+    printf("---------------------------------------------------\n");
+    // for each symbol print kind, name, value, level, address, and mark
+    for(int i = 0; i < symbCount; i++){
+        printf(" %4d| %11s| %5d| %6d|  %7d|%5d\n", symbol_table[i].kind, symbol_table[i].name, symbol_table[i].val, symbol_table[i].level, symbol_table[i].addr, symbol_table[i].mark);
+        
+    }
+
+}
+
+// Helper functions
+char * determineOpcode(int i) {
+    // determine instruction name based on its opcode
+    if (instructionSet[i].OP == 1) {
+        return "LIT";
+    }
+    if (instructionSet[i].OP == 2) {
+        return "OPR";
+    }
+    else if (instructionSet[i].OP == 3) {
+        return "LOD";
+    }
+    else if (instructionSet[i].OP == 4) {
+        return "STO";
+    }
+    else if (instructionSet[i].OP == 5) {
+        return "CAL";
+    }
+    else if (instructionSet[i].OP == 6) {
+        return "INC";
+
+    }
+    else if (instructionSet[i].OP == 7) {
+        return "JMP";
+    }
+    else if (instructionSet[i].OP == 8) {
+        return "JPC";
+    }   
+    return "SYS";
+}
+
+void getNextToken () {
+    // scans for next token in input file
+    fscanf(fp, "%d", &nextToken);
+    // if next token is 1, call skipsym error
+    if (nextToken == 1) {
+        error(0);
+    }
+}
+
+// called when there's an error
+void error (int errorNumber) {
+    char * errors [20] = {"Error: Scanning error detected by lexer (skipsym present)", "Error: program must end with period", "Error: const, var, and read keywords must be followed by identifier", 
+        "Error: symbol name has already been declared", "Error: constants must be assigned with =", "Error: constants must be assigned an integer value", 
+        "Error: constant and variable declarations must be followed by a semicolon", "Error: undeclared identifier", "Error: only variable values may be altered", "Error: assignment statements must use :=",
+        "Error: begin must be followed by end", "Error: if must be followed by then", "Error: while must be followed by do", "Error: condition must contain comparison operator", "Error: right parenthesis must follow left parenthesis", 
+        "Error: arithmetic equations must contain operands, parentheses, numbers, or symbols", "Error: code index exceeded code length", "Error: can't access symbol because mark set to one", "Error: program doesn't handle procedures", "Error: fi must follow if statement"};
+    deleteAll();
+    printf("%s\n", errors[errorNumber]);
+    fprintf(outputFile, "%s\n", errors[errorNumber]);
+    exit(EXIT_FAILURE);
+}
+
+int main (int argc, char *argv[])
+{
+    // No arguments accepted
+    if(argc != 1){
+        printf("Error! Wrong number of arguments.\n");
+        return 1;
+    } 
+
+    // hard coded instruction to jump to program
+    emit(7, 0, 3);
+    // open input and output files
+    fp = fopen("tokens.txt", "r");
+    outputFile = fopen("elf.txt", "w");
+
+    //global variable declaration
+    getNextToken();
+    tp = 0;
+    symbCount = 0;
+
+    program();
+        
+    deleteAll();
+    
+    printAssemblyCode();
+    printSymbolTable();
+    exit(EXIT_SUCCESS);
+}
---
+++ vm.c
@@ -0,0 +1,333 @@
+
+/*
+Assignment:
+HW4 - Complete Parser and Code Generator for PL/0
+(with Procedures, Call, and Else)
+
+Author(s): Sindhuja Sesham, Mikayla Philpot
+
+Language: C (only)
+
+To Compile:
+
+Scanner:
+gcc -O2 -std=c11 -o lex lex.c
+
+Parser/Code Generator:
+gcc -O2 -std=c11 -o parsercodegen_complete parsercodegen_complete.c
+
+Virtual Machine:
+gcc -O2 -std=c11 -o vm vm.c
+
+To Execute (on Eustis):
+./lex <input_file.txt>
+./parsercodegen_complete
+./vm elf.txt
+
+where:
+<input_file.txt> is the path to the PL/0 source program
+
+Notes:
+- lex.c accepts ONE command-line argument (input PL/0 source file)
+- parsercodegen_complete.c accepts NO command-line arguments
+- Input filename is hard-coded in parsercodegen_complete.c
+- Implements recursive-descent parser for extended PL/0 grammar
+- Supports procedures, call statements, and if-then-else
+- Generates PM/0 assembly code (see Appendix A for ISA)
+- VM must support EVEN instruction (OPR 0 11)
+- All development and testing performed on Eustis
+
+Class: COP3402 - System Software - Fall 2025
+
+Instructor: Dr. Jie Lin
+
+Due Date: Friday, November 21, 2025 at 11:59 PM ET
+*/
+#include <stdio.h>
+
+// Create IR struct
+struct instruction{
+    int OP; 
+    int L;
+    int M;
+};
+
+int PAS [500] = {0};
+
+// instruction register
+struct instruction IR;
+// Registers
+int PC, BP, SP;
+
+// BASE FUNCTION
+int base ( int BP , int L ){
+    int arb = BP ; // activation record base
+    while ( L > 0) {
+        arb = PAS [ arb ]; // follow static link
+        L --;
+    }
+    return arb ;
+}
+
+/* Print out the PAS */
+void print(int PAS[500]) {
+    // label instructions based on opcode
+    if(IR.OP == 1) {
+        printf("\nLIT");
+    }
+    if(IR.OP == 2) {
+        // label opcode 2 instructions based on memory
+        if(IR.M == 0)
+            printf("\nRTN");
+        else if(IR.M == 1)
+            printf("\nADD");
+        else if(IR.M == 2)
+            printf("\nSUB");
+        else if (IR.M == 3)
+            printf("\nMUL");
+        else if (IR.M == 4)
+            printf("\nDIV");
+        else if (IR.M == 5)
+            printf("\nEQL");
+        else if (IR.M == 6)
+            printf("\nNEQ");
+        else if (IR.M == 7) {
+            printf("\nLSS");
+        }
+        else if (IR.M == 8) {
+            printf("\nLEQ");
+        }
+        else if (IR.M == 9) {
+            printf("\nGTR");
+        }
+        else if (IR.M == 10) {
+            printf("\nGEQ");
+        }
+    }
+    if(IR.OP == 3) {
+        printf("\nLOD");
+    }
+    if(IR.OP == 4) {
+        printf("\nSTO");
+    }
+    if(IR.OP == 5) {
+        printf("\nCAL");
+    }
+    if(IR.OP == 6) {
+        printf("\nINC");
+       
+    }
+    if(IR.OP == 7) {
+        printf("\nJMP");
+    }
+    if(IR.OP == 8) {
+        printf("\nJPC");
+    }
+    if(IR.OP == 9) {
+        printf("\nSYS");
+
+        
+    }
+    // print current register values
+    printf("\t%d \t%d \t%d \t%d \t%d \t", IR.L, IR.M, PC, BP, SP);
+    // print current stack
+    int arb = BP;
+    int prevLevel = 0;
+    while(arb < 439) {
+        /*for (int i = arb; SP <= i; i--) {
+        printf("%d ",  PAS[i]);
+        
+        }*/
+        prevLevel = arb;
+        arb = PAS[arb - 1];
+    }
+    for (int i = arb; SP <= i; i--) {
+        if (prevLevel != 0 && prevLevel == i) {
+            printf("| ");
+        }
+        printf("%d ",  PAS[i]);
+        
+    }
+    printf("\n\n");
+}
+
+int main(int argc, char *argv[]) {
+
+    // Checking argument count
+    if(argc != 2){
+        printf("Error! Wrong number of arguments.");
+        return 1;
+    }
+    
+    // File handling
+    FILE *inputFile = fopen(argv[1], "r");
+
+    if (inputFile == NULL) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+    
+    // Scanning instructions into PAS
+    int i = 499;
+    
+    while(fscanf(inputFile,"%d",&PAS[i]) == 1) {
+            i--;
+    }
+
+    // Setting Global Variables
+    PC = 499;
+    SP = i + 1;
+    BP = SP - 1;
+    printf("\tL \tM \tPC \tBP \tSP \tstack\n");
+    printf("\nInitial values: \t%d \t%d \t%d\n", PC, BP, SP);
+    while(1){
+        
+        // Setting Instruction Register
+        IR.OP = PAS[PC];
+        IR.L = PAS[PC-1];
+        IR.M = PAS[PC-2];
+        
+        // Decrementing PC
+        PC = PC - 3;
+
+        // Lit Instruction
+        if (IR.OP == 1) {
+            SP = SP - 1;  
+            PAS[SP] = IR.M;
+            print(PAS);
+        }
+        // OPR Instruction
+        else if (IR.OP == 2) {
+            
+            // RTN
+            if(IR.M == 0){
+                
+                SP = BP + 1;
+                BP = PAS[SP - 2];
+                PC = PAS[SP - 3];
+
+            // ADD
+            } else if(IR.M == 1){
+                PAS[SP + 1] = PAS [SP + 1] + PAS[SP];
+                SP = SP + 1;
+            // SUB
+            } else if(IR.M == 2){
+                PAS[SP + 1] = PAS [SP + 1] - PAS[SP];
+                SP = SP + 1;
+            // MUL
+            } else if(IR.M == 3){
+                PAS[SP + 1] = PAS [SP + 1] * PAS[SP];
+                SP = SP + 1;
+            // DIV
+            } else if(IR.M == 4){
+                PAS[SP + 1] = PAS [SP + 1] / PAS[SP];
+                SP = SP + 1;
+            // EQL
+            } else if(IR.M == 5){
+                PAS[SP + 1] = (PAS [SP + 1] == PAS[SP]);
+                SP = SP + 1;
+            // NEQ
+            } else if(IR.M == 6){
+                if(PAS[SP+1] != PAS[SP])
+                    PAS[SP+1] = 1;
+                else    
+                    PAS[SP+1] = 0;
+                SP++;
+            // LSS
+            } else if(IR.M == 7){
+                if(PAS[SP+1] < PAS[SP])
+                    PAS[SP+1] = 1;
+                else    
+                    PAS[SP+1] = 0;
+                SP++;
+            // LEQ
+            } else if(IR.M == 8){
+                if(PAS[SP+1] <= PAS[SP])
+                    PAS[SP+1] = 1;
+                else    
+                    PAS[SP+1] = 0;
+                SP++;
+            // GTR
+            } else if(IR.M == 9){
+                if(PAS[SP+1] > PAS[SP])
+                    PAS[SP+1] = 1;
+                else    
+                    PAS[SP+1] = 0;
+                SP++;
+            // GEQ
+            } else if(IR.M == 10){
+                if(PAS[SP+1] >= PAS[SP])
+                    PAS[SP+1] = 1;
+                else    
+                    PAS[SP+1] = 0;
+                SP++;
+            } 
+            print(PAS);
+        }
+        // LOD Instruction
+        else if (IR.OP == 3) {
+            SP = SP - 1;
+            PAS[SP] = PAS[base(BP, IR.L) - IR.M];
+            print(PAS);
+        }
+        // STO Instruction 
+        else if (IR.OP == 4) {
+            // Store procedure
+            PAS[base(BP, IR.L) - IR.M] = PAS[SP];
+            SP++;
+            print(PAS);
+        }
+        else if (IR.OP == 5) {
+            // Call procedure
+            PAS[SP - 1] = base(BP, IR.L);
+            PAS[SP - 2] = BP;
+            PAS[SP - 3] = PC;
+            BP = SP - 1;
+            PC = 499 - IR.M;
+            print(PAS);
+        }
+        else if (IR.OP == 6) {
+            // Allocating memory on the stack
+            SP = SP - IR.M;
+            print(PAS);
+        }
+        else if (IR.OP == 7) {  
+            // Unconditional jumps
+            PC = 499 - IR.M;
+            print(PAS);
+        }
+        else if (IR.OP == 8) {
+            // Conditional jumps
+            if(PAS[SP] == 0) {
+                PC = 499 - IR.M;
+            }
+            SP = SP + 1;
+            print(PAS);
+        }
+        else if (IR.OP == 9) {
+            if(IR.M== 1){
+                // Output integer value at top of stack, then pop
+                printf("Output result is: %d\n", PAS[SP]);
+                SP = SP + 1;
+            } else if(IR.M== 2){
+                // Read an integer from stdin and push it
+                SP = SP - 1;
+                printf("\nPlease Enter an Integer: ");
+                scanf("%d, ", &PAS[SP]);
+
+                
+            }
+            else if (IR.M == 3) {
+                print(PAS);
+                // Halt program
+                break;
+            }
+            print(PAS);
+        }
+        
+        
+    }
+    
+    fclose(inputFile);
+
+}
